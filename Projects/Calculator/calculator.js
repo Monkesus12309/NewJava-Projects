c52a2f6e42d5d813b447a0fc78f53714b328439e
Monkesus12309 <104838465+Monkesus12309@users.noreply.github.com>
2026-08-19 12:24:37 +0100

    calulator html and css is done

---
+++ Projects/Calculator/calculator.js
@@ -0,0 +1,11 @@
+// Creats an object to keep track of values.
+const Calculator = {
+    // this will display 0 on the calculator screen.
+    Display_Value: '0',
+    // this will hold the first operand for any expressions, we set it to null for now.
+    First_Operand: null,
+    // this checks wether or not the second operand has been inputted by the user.
+    Wait_Second_Operand: false,
+    // this will hold the operator, we set it to null for now.
+    operator: null,
+};
